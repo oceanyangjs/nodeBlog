a158fb918820575e70f3fca6aeecfab46fa38a71
yangjingsheng <hityjsforever@126.com>
2018-03-07 18:39:32 +0800

    增加登陆首页分类信息数据库查询

--- routers/main.js
+++ routers/main.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var router = express.Router()
+var Category = require("../models/Category")
 
 router.get('/user',function(req,res,next){
 	res.send('main - User');
@@ -8,9 +9,15 @@ router.get('/user',function(req,res,next){
 router.get('/',function(req,res,next){
 	console.log('访问主页')
 	console.log(req.userInfo)
-	res.render('main/index',{
-		userInfo:req.userInfo
-	});
+
+	Category.find(function(err,categories){
+		console.log(categories)
+		res.render('main/index',{
+			userInfo:req.userInfo,
+			categories:categories
+		});
+	})
+
 })
 
 
